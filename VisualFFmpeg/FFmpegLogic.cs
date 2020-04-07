125671c7896eb7af3b38a9d167d27b7106fcdcdb
John Sorial <jsorial@students.uni-mainz.de>
2020-04-07 19:56:32 +0200

    Add project files.

---
+++ VisualFFmpeg/FFmpegLogic.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace VisualFFmpeg
+{
+    public class PercentageEventArgs : EventArgs
+    {
+        public double percentage { set; get; }
+        public PercentageEventArgs(double percentage)
+        {
+            this.percentage = percentage;
+        }
+
+        public override string ToString()
+        {
+            return percentage.ToString();
+        }
+    }
+    
+
+    class ffmpegHandeler
+    {
+        public string FFmpegPath { set; get; }
+        public string Arguments { set; get; }
+        public string InputFile { set; get; }
+        public string OutputFIle { set; get; }
+        private static TimeSpan duration;     
+        
+
+        public TimeSpan getduration()
+        {
+            return duration;
+        }
+
+
+        public event EventHandler PercantageChange; 
+
+
+        static TimeSpan convertRegexToSpan(string input)
+        {
+            var splitted = input.Split(":");
+            int[] ints = Array.ConvertAll(splitted, int.Parse);
+            TimeSpan Duration = new TimeSpan(ints[0], ints[1], ints[2]);
+            return Duration;
+        }
+        private  void MyProcOutputHandler(object sendingProcess,
+           DataReceivedEventArgs e)
+        {
+            // Collect the sort command output. 
+            if (!String.IsNullOrEmpty(e.Data))
+            {
+                Regex getDuration = new Regex(@"Duration: ((?:\d{2}(?:\:|)){3})", RegexOptions.IgnoreCase | RegexOptions.IgnoreCase);
+                Regex proccedTime = new Regex(@"time=((?:\d{2}(?:\:|)){3})", RegexOptions.IgnoreCase | RegexOptions.IgnoreCase);
+                Console.WriteLine(e.Data);
+                MatchCollection matches = getDuration.Matches(e.Data);
+                if (matches.Count != 0)
+                {
+                    Console.WriteLine("Duration of File is {0}", matches[0].Groups[1]);
+                    duration = convertRegexToSpan(matches[0].Groups[1].ToString());
+
+                }
+                matches = proccedTime.Matches(e.Data);
+                if (matches.Count != 0)
+                {
+                    Console.WriteLine("Procced Time {0}", matches[0].Groups[1]);
+                    TimeSpan elapsed = convertRegexToSpan(matches[0].Groups[1].ToString());
+                    double done = (elapsed.TotalSeconds / duration.TotalSeconds) * 100;
+                    Console.WriteLine("{0:N2}% Done", done);
+                    OnProgressUpdate(done);
+                    
+
+
+
+                }
+            }
+        }
+        public ffmpegHandeler(string ffmpegpath,string inputFilePath,string outputFilePath)
+        {
+            this.FFmpegPath = ffmpegpath;
+            this.InputFile = inputFilePath;
+            this.OutputFIle = outputFilePath;
+
+            
+        }
+
+        protected virtual void OnProgressUpdate(double percentage)
+        {
+            if(PercantageChange != null) {
+                PercantageChange(this, new PercentageEventArgs(percentage));
+            }
+        }
+
+        
+        public void StartConverion(){
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = FFmpegPath,
+                    Arguments = @"-i D:\pointer.mp4 D:\output.mp3",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardInput = true
+                }
+            };
+            process.ErrorDataReceived += new DataReceivedEventHandler(MyProcOutputHandler);
+            process.Start();
+            Console.WriteLine("Started Coversion");
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+            process.Close();
+        }
+    }
+}
---
+++ VisualFFmpeg/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace VisualFFmpeg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string SavePath = "";
+        static double currentPercentage = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void loadffmpeg_btn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog loadFile = new OpenFileDialog();
+            loadFile.Title = "Select FFmpeg Path";
+            loadFile.Filter = "ffmpeg |ffmpeg.exe";
+            loadFile.InitialDirectory = "D:\\";
+            loadFile.ShowDialog();
+            ffmpegPathLabel.Content = loadFile.FileName;
+        }
+
+        private void loadFile_btn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog loadFile = new OpenFileDialog();
+            loadFile.Title = "Select FFmpeg Path";
+            loadFile.InitialDirectory = "D:\\";
+            loadFile.ShowDialog();
+            loadFile_lbl.Content = loadFile.FileName;
+        }
+
+        private void saveFile_btn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Title = "Save As";
+            saveFile.ShowDialog();
+            SavePath = saveFile.FileName;
+            MessageBox.Show(SavePath);
+
+        }
+
+        private void convert_btn_Click(object sender, RoutedEventArgs e)
+        {
+            convert_btn.IsEnabled = false;
+            Thread ffmpeg = new Thread(ffmpegAction);
+
+            ffmpeg.Start(ffmpegPathLabel.Content);
+        }
+
+        private void ffmpegAction(object path)
+        {
+
+            ffmpegHandeler ffmpeg = new ffmpegHandeler((string)path);
+            ffmpeg.PercantageChange += update_load;
+            ffmpeg.StartConverion();
+            convert_btn.Dispatcher.BeginInvoke((Action)(() => convert_btn.IsEnabled = true));
+            
+        }
+
+
+
+        private void update_load(object sender, EventArgs e)
+        {
+
+            percent_lbl.Dispatcher.BeginInvoke((Action)(() => percent_lbl.Content = e.ToString()));
+            percent_bar.Dispatcher.BeginInvoke((Action)(() => percent_bar.Value = Convert.ToDouble(e.ToString())));
+
+        }
+    }
+
+
+}
